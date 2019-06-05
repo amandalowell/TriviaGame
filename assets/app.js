34ad12096da439469c11444fdf6449a88d3362f4
Amanda Lowell <amandalowell2016@gmail.com>
2019-06-05 18:12:25 -0500

    java

---
+++ assets/app.js
@@ -0,0 +1,21 @@
+// time for each question
+var time = 15;
+
+// # of questions user got right
+var correctGuesses = 0;
+
+// # of questions user got right
+var incorrectGuesses = 0;
+
+// which question the user is on
+var questionCounter = 0;
+
+
+//things i need :
+// variable to start game ( click button called Start)
+//need a timeout function for timer (call it time reamaining)
+//need a reset function for when you want to start a new game/ end old game
+//click button for "done"
+//function to get you to the final screen to show : correct ans., incorrect ans.
+
+//questions: how to make seperate pages....do i hide start button/ how does that html look
